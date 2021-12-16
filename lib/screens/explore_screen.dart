79224062912d2978b67f6de0f3b4587e48485d08
SeongHoon Wee <mrgravity817@gmail.com>
2021-12-16 10:19:09 +0900

    Add conditional explore cards

---
+++ lib/screens/explore_screen.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import '../models/explore_data.dart';
+import '../api/mock_fooderlich_service.dart';
+import '../components/components.dart';
+
+class ExploreScreen extends StatelessWidget {
+  final mockService = MockFooderlichService();
+
+  ExploreScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: mockService.getExploreData(),
+        builder: (context, AsyncSnapshot<ExploreData> snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final recipes = snapshot.data?.todayRecipes ?? [];
+            return TodayRecipeListView(recipes: recipes);
+          } else {
+            return const Center(child: CircularProgressIndicator());
+          }
+        });
+  }
+}
